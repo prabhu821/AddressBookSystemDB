417cc89af31e8e3e9fdc393e9331d06e716bf379
rishiraj007 <rishiraj@mounttalent.com>
2023-04-19 20:09:30 +0530

    [Prabhu]Add: create table

--- AddressBookSystemDB/AddressBookDB.sql
+++ AddressBookSystemDB/AddressBookDB.sql
@@ -1,3 +1,8 @@
 ﻿--UC1 Create Address Book Service Database
 create database Address_Book_Service;
 use Address_Book_Service;
+
+--UC2 create a Address Book table
+create Table Address_Book
+(First_Name varchar(100), Last_Name varchar(100), Address varchar(100), City varchar(50), State varchar(50), 
+Zip_Code int, Phone_Number bigint, Email_Id varchar(100));
