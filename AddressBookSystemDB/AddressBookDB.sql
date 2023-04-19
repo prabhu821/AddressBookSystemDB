2292d63237b5af5c7366d61c9806d8bc0ac9e789
rishiraj007 <rishiraj@mounttalent.com>
2023-04-19 20:10:52 +0530

    [Prabhu]Add: insert contacts

--- AddressBookSystemDB/AddressBookDB.sql
+++ AddressBookSystemDB/AddressBookDB.sql
@@ -6,3 +6,13 @@ use Address_Book_Service;
 create Table Address_Book
 (First_Name varchar(100), Last_Name varchar(100), Address varchar(100), City varchar(50), State varchar(50), 
 Zip_Code int, Phone_Number bigint, Email_Id varchar(100));
+
+--UC3 Ability to insert new Contacts to Address Book
+insert into Address_Book values
+('Anmol', 'Roy', 'MurariNagar', 'Khurja', 'UP', 203131, 7805982523, 'anmol122@gmail.com'),
+('Riya', 'Sharma', 'PreetViharColony', 'Agra','UP', 784512, 9823659825, 'riya25@gmail.com'),
+('Piyush', 'Rao', 'VijayNagar', 'Noida', 'UP', 789456, 7502982523, 'piyus2504@gmail.com'),
+('Amit', 'Singh', 'ShastriNagar', 'Meerut', 'UP', 258963, 9639026598, 'amit25@gmail.com'),
+('Monika', 'Sharma', 'NewTehisl', 'Ghaziabad', 'UP', 898963, 8839026598, 'monika25@gmail.com'),
+('Manisha', 'Gupta', 'KalindiKunj', 'Aligarh', 'UP', 357895, 7836152489, 'mahi258@gmail.com');
+
